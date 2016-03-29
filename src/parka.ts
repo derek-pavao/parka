7b4d42861d2d09bd1a7855ce966c24798ba3b85b
Derek Pavao <derek.pavao@nextsteplivinginc.com>
2016-03-29 19:24:19 -0400

    Forgot to export @TableName()

--- src/parka.ts
+++ src/parka.ts
@@ -10,5 +10,5 @@ export {PUT} from './decorators/put';
 export {DELETE} from './decorators/delete';
 export {RequestBody} from './decorators/request-body';
 export {ParkaModel} from './core/parka-model';
-
+export {TableName} from './decorators/table-name';
 
