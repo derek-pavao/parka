681e21964867fd88383f4b202604470cd0e7319a
Derek Pavao <derek@derekpavao.com>
2016-10-05 21:28:50 -0400

    Fixed some dependencies and got the app running

--- test/example/models/status-model.ts
+++ test/example/models/status-model.ts
@@ -1,11 +1,24 @@
 // import {ParkaModel} from "../../../src/core/parka-model";
-// import {TableName} from "../../../src/decorators/table-name";
-// import {Model} from "../../../src/decorators/model";
-//
-// @Model()
-// @TableName('Status')
-// export class StatusModel {
-//   status: string;
-//   statusMessage: string;
-//
-// }
+import * as sequelize from 'sequelize';
+ import {TableName} from "../../../src/decorators/table-name";
+ import {Model} from "../../../src/decorators/model";
+
+ @Model()
+ export class StatusModel {
+
+   public static definition = {
+     attributes: {
+       id: {
+         type: sequelize.INTEGER,
+         primaryKey: true,
+         autoIncrement: true
+       },
+       status: sequelize.STRING,
+       statusMessage: sequelize.STRING
+     }
+   };
+
+   public status: string;
+   public statusMessage: string;
+
+ }
